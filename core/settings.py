250775d5cfb5cd678e64deb66504a59258630024
Abdullah Yasin <abdullahyasin4656@gmail.com>
2024-10-10 12:26:37 +0500

    ersgdf

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'daphne',
     'app'
 ]
 
+
+ASGI_APPLICATION = "core.asgi.application"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
